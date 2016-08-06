8aa29260f3dc9bb00d73c2392cfea97e15e72b0a
ssamgir <amitbobade2007@gmail.com>
2016-08-06 16:17:39 +0530

    first commit

---
+++ SSBasicCalculator/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  SSBasicCalculator
+//
+//  Created by Amit Bobade on 06/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+- (IBAction)button0:(id)sender;
+
+- (IBAction)button1:(id)sender;
+- (IBAction)button2:(id)sender;
+- (IBAction)button3:(id)sender;
+- (IBAction)button4:(id)sender;
+@property (strong, nonatomic) IBOutlet UIButton *button5;
+
+@end
+
---
+++ SSBasicCalculator/ViewController.m
@@ -0,0 +1,41 @@
+//
+//  ViewController.m
+//  SSBasicCalculator
+//
+//  Created by Amit Bobade on 06/08/16.
+//  Copyright © 2016 Sandesh Samgir. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)button0:(id)sender {
+}
+
+- (IBAction)button1:(id)sender {
+}
+
+- (IBAction)button2:(id)sender {
+}
+
+- (IBAction)button3:(id)sender {
+}
+
+- (IBAction)button4:(id)sender {
+}
+@end
