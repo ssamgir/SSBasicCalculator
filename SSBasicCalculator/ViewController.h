efb06e45a9080395f4d1842c0be9922e031df552
Sandesh Samgir <samgirsan@outlook.com>
2016-08-13 19:49:15 +1000

    updated ui and logic

--- SSBasicCalculator/ViewController.h
+++ SSBasicCalculator/ViewController.h
@@ -8,14 +8,41 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-- (IBAction)button0:(id)sender;
+typedef enum{ Plus,Minus,Multiply,Divide} performOperation;
 
+
+@interface ViewController : UIViewController
+{
+
+    
+    IBOutlet UITextField *displayText;
+
+    IBOutlet UIButton *buttonClear;
+
+    NSString * stoareResult;
+    
+    performOperation operation;
+    
+}
+
+- (IBAction)button0:(id)sender;
 - (IBAction)button1:(id)sender;
 - (IBAction)button2:(id)sender;
 - (IBAction)button3:(id)sender;
 - (IBAction)button4:(id)sender;
-@property (strong, nonatomic) IBOutlet UIButton *button5;
+- (IBAction)button5:(id)sender;
+- (IBAction)button6:(id)sender;
+- (IBAction)button7:(id)sender;
+- (IBAction)button8:(id)sender;
+- (IBAction)button9:(id)sender;
+- (IBAction)buttonplus:(id)sender;
+
+- (IBAction)buttonMulti:(id)sender;
+- (IBAction)buttonminus:(id)sender;
+- (IBAction)buttonDivide:(id)sender;
+
+- (IBAction)buttonEqual:(id)sender;
+
 
 @end
 
